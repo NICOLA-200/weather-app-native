b56aa9692f5046e24d70d3f5ae4f970f3b681c8a
NICOLA-200 <fatepepe66@gmail.com>
2023-12-01 09:16:05 +0200

    the  location feature added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -111,3 +111,25 @@ export default function HomeScreen() {
                    </TouchableOpacity>
                  </View>
                  {
+                                      locations.length>0 && showSearch?(
+                                        <View className="absolute w-full bg-gray-300 top-16 rounded-3xl ">
+                                          {
+                                            locations.map((loc, index)=>{
+                                              let showBorder = index+1 != locations.length;
+                                              let borderClass = showBorder? ' border-b-2 border-b-gray-400':'';
+                                              return (
+                                                <TouchableOpacity 
+                                                  key={index}
+                                                  onPress={()=> handleLocation(loc)} 
+                                                  className={"flex-row items-center border-0 p-3 px-4 mb-1 "+borderClass}>
+                                                    <MapPinIcon size="20" color="gray" />
+                                                    <Text className="text-black text-lg ml-2">{loc?.name}, {loc?.country}</Text>
+                                                </TouchableOpacity>
+                                              )
+                                            })
+                                          }
+                                        </View>
+                                      ):null
+                                    }
+                                    
+                                  </View>
