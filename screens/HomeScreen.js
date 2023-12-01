f6084988a9d869fb291e5338c4be4e2a29b30e3d
NICOLA-200 <fatepepe66@gmail.com>
2023-12-01 09:14:12 +0200

    home screen added

---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import { View, Text, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { MagnifyingGlassIcon, XMarkIcon } from 'react-native-heroicons/outline'
+import { CalendarDaysIcon, MapPinIcon } from 'react-native-heroicons/solid'
+import { debounce } from "lodash";
+import { theme } from '../theme';
+import { fetchLocations, fetchWeatherForecast } from '../api/weather';
+import * as Progress from 'react-native-progress';
+import { StatusBar } from 'expo-status-bar';
+import { weatherImages } from '../constants';
+import { getData, storeData } from '../utils/asyncStorage';
+
+export default function HomeScreen() {
+  const [showSearch, toggleSearch] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [weather, setWeather] = useState({})
